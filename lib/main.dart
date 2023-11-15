e742f9203ea5df396c77fb7622b3a6ec976607d3
GhanyR <neoperkasa@gmail.com>
2023-11-15 19:39:30 +0700

    Tutorial 8

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
+import 'package:shopping_list/screens/login.dart';
 import 'package:shopping_list/screens/menu.dart';
 
 void main() {
@@ -10,12 +13,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.indigo),
-      ),
-      home: MyHomePage(),
+    return Provider(
+      create: (_) {
+        CookieRequest request = CookieRequest();
+        return request;
+      },
+      child: MaterialApp(
+          title: 'Flutter App',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+            useMaterial3: true,
+          ),
+          home: const LoginPage()),
     );
   }
 }
--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list/screens/menu.dart'; 
-import 'package:shopping_list/screens/shoplist_form.dart'; 
+import 'package:flutter/material.dart';
+import 'package:shopping_list/screens/menu.dart';
+import 'package:shopping_list/screens/shoplist_form.dart';
+import 'package:shopping_list/screens/list_product.dart';
 
 class LeftDrawer extends StatelessWidget {
-  const LeftDrawer({Key? key}) : super(key: key); 
+  const LeftDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
         children: [
-          DrawerHeader(
-            decoration: const BoxDecoration(
+          const DrawerHeader(
+            decoration: BoxDecoration(
               color: Colors.indigo,
             ),
             child: Column(
-              children: const [
+              children: [
                 Text(
                   'Shopping List',
                   textAlign: TextAlign.center,
@@ -26,37 +28,46 @@ class LeftDrawer extends StatelessWidget {
                   ),
                 ),
                 Padding(padding: EdgeInsets.all(10)),
-                Text(
-                  "Catat seluruh keperluan belanjamu di sini!",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.normal,
-                    color: Colors.white,
-                  ),
-                ),
+                Text("Catat seluruh keperluan belanjamu di sini!",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.white,
+                    )),
               ],
             ),
           ),
           ListTile(
             leading: const Icon(Icons.home_outlined),
             title: const Text('Halaman Utama'),
+            // Bagian redirection ke MyHomePage
             onTap: () {
               Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => MyHomePage(), 
-                ),
-              );
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MyHomePage(),
+                  ));
             },
           ),
           ListTile(
             leading: const Icon(Icons.add_shopping_cart),
             title: const Text('Tambah Produk'),
+            // Bagian redirection ke ShopFormPage
             onTap: () {
               Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const ShopFormPage()));
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.shopping_basket),
+            title: const Text('Daftar Produk'),
+            onTap: () {
+              // Route menu ke halaman produk
+              Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => ShopFormPage()),
+                MaterialPageRoute(builder: (context) => const ProductPage()),
               );
             },
           ),
